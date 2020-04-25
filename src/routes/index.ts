9a145c7487ac6de0259bb4994ef041a99bdd9385
Maick Souza <maick_a_s@msn.com>
2020-04-25 14:26:18 -0300

    feat(server): vscode debugging settings

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,8 +2,17 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) =>
-  response.json({ message: 'Hello GoStack' }),
-);
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = {
+    name,
+    email,
+  };
+
+  return response.json(user);
+
+  // response.json({ message: 'Hello GoStack' }),
+});
 
 export default routes;
